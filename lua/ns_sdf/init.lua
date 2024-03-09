0300baaba44e9e2ddaabd223fcac5380515cd111
Nicholas Cameron <nicholas.cameron@techfino.com>
2024-03-09 19:29:53 -0300

    file test added

--- lua/ns_sdf/init.lua
+++ lua/ns_sdf/init.lua
@@ -4,4 +4,23 @@ function M.setup()
 	print("setup function")
 end
 
+-- Check if a file exists in the current directory
+local function fileExists(filename)
+	local stat = vim.loop.fs_stat(filename)
+	return stat and stat.type == "file"
+end
+
+-- Get the current working directory
+local function getCurrentDirectory()
+	return vim.fn.getcwd()
+end
+
+-- Usage example
+local filename = getCurrentDirectory() .. "/" .. "project.json"
+if fileExists(filename) then
+	print("File exists:", filename)
+else
+	print("File does not exist:", filename)
+end
+
 return M
