58452b195e122835dfea9088b20c4e5ceb594e4e
Nicholas Cameron <nicholas.cameron@techfino.com>
2024-03-10 22:46:04 -0300

    showEnvironment

--- lua/ns_sdf/init.lua
+++ lua/ns_sdf/init.lua
@@ -1,6 +1,8 @@
 local M = {}
 
 function M.setup()
+	vim.api.nvim_set_option("statusline", "Your text goes here")
+
 	print("setup function")
 end
 
@@ -18,17 +20,38 @@ function M.getCurrentDir()
 	return vim.fn.expand("%:p:h")
 end
 
-function M.checkFile()
+-- Define a function to extract the value associated with the "defaultAuthId" key
+function M.extractDefaultAuthId(filename)
+	-- Open the project.json file in read mode
+	local file = io.open(filename, "r")
+	if not file then
+		print("Error: Unable to open " .. filename)
+		return nil
+	end
+	-- Read the content of the file
+	local content = file:read("*all")
+	-- Close the file
+	file:close()
+	-- Parse the content as JSON
+	local json = require("json")
+	local data = json.decode(content)
+	-- Extract the value associated with the "defaultAuthId" key
+	local defaultAuthId = data.defaultAuthId
+	return defaultAuthId
+end
+
+function M.showEnvironment()
 	local file = M.getCurrentDir() .. "/project.json"
 	if M.file_exists(file) then
-		print("file exists")
+		local defaultAuthId = M.extractDefaultAuthId(file)
+		print(defaultAuthId)
 	else
 		print("file does not exist")
 	end
 end
 
 function M.configure_keymaps()
-	vim.cmd("command! SdfEnv :lua require'ns_sdf'.checkFile()")
+	vim.cmd("command! SdfEnv :lua require'ns_sdf'.showEnvironment()")
 end
 
 return M
